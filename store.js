869955ae8bb52cccd9cf4aaaf610f649021cc23c
WOOWOOYONG <woowooyong@gmail.com>
2022-07-03 09:50:57 +0800

    增加購物車功能註解

--- store.js
+++ store.js
@@ -1,23 +1,25 @@
+//購物車商品移除按鈕
 const removeCartItemButtons = document.querySelectorAll(".btn-danger");
 
 for (let i = 0; i < removeCartItemButtons.length; i++) {
   let button = removeCartItemButtons[i];
   button.addEventListener("click", removeCartItem);
 }
-
+//購物車商品數量變更按鈕
 const quantityInputs = document.querySelectorAll(".cart-quantity-input");
 for (let i = 0; i < quantityInputs.length; i++) {
   let input = quantityInputs[i];
   input.addEventListener("change", quantityChanged);
 }
 
-//加入購物車按鈕
+//商品加入購物車按鈕
 const addToCartButtons = document.querySelectorAll(".shop-item-button");
 for (let i = 0; i < addToCartButtons.length; i++) {
   let button = addToCartButtons[i];
   button.addEventListener("click", addToCartClicked);
 }
 
+//結帳後清空購物車更新金額
 document
   .querySelector(".btn-purchase")
   .addEventListener("click", purchaseClicked);
@@ -31,37 +33,38 @@ function purchaseClicked() {
   updateCartTotal();
 }
 
+//將商品從購物車移除
 function removeCartItem(e) {
-  let buttonClicked = e.target;
+  const buttonClicked = e.target;
   buttonClicked.parentElement.parentElement.remove();
   updateCartTotal();
 }
 
+//購物車數量不可小於1
 function quantityChanged(e) {
-  let input = e.target;
+  const input = e.target;
   if (isNaN(input.value) || input.value <= 0) {
     input.value = 1;
   }
   updateCartTotal();
 }
 
+//將商品加入購物車
 function addToCartClicked(e) {
-  let button = e.target;
-  let shopItem = button.parentElement.parentElement;
-  let title = shopItem.querySelector(".shop-item-title").innerText;
-  let price = shopItem.querySelector(".shop-item-price").innerText;
-  let imageSrc = shopItem.querySelector(".shop-item-image").src;
-  console.log(title, price, imageSrc);
+  const button = e.target;
+  const shopItem = button.parentElement.parentElement;
+  const title = shopItem.querySelector(".shop-item-title").innerText;
+  const price = shopItem.querySelector(".shop-item-price").innerText;
+  const imageSrc = shopItem.querySelector(".shop-item-image").src;
   addItemToCart(title, price, imageSrc);
   updateCartTotal();
 }
 
-//將商品加入購物車
 function addItemToCart(title, price, imageSrc) {
-  let cartRow = document.createElement("div");
+  const cartRow = document.createElement("div");
   cartRow.classList.add("cart-row");
-  let cartItems = document.querySelector(".cart-items");
-  let cartItemNames = cartItems.querySelectorAll(".cart-item-title");
+  const cartItems = document.querySelector(".cart-items");
+  const cartItemNames = cartItems.querySelectorAll(".cart-item-title");
   for (let i = 0; i < cartItemNames.length; i++) {
     if (cartItemNames[i].innerText == title) {
       alert("This item is already added to the cart");
@@ -69,7 +72,7 @@ function addItemToCart(title, price, imageSrc) {
     }
   }
 
-  let cartRowContents = `
+  const cartRowContents = `
   <div class="cart-item cart-column">
   <img class="cart-item-image" src="${imageSrc}" width="100" height="100" >
   <span class="cart-item-title">${title}</span>
@@ -89,9 +92,10 @@ function addItemToCart(title, price, imageSrc) {
     .addEventListener("change", quantityChanged);
 }
 
+//更新購物車金額
 function updateCartTotal() {
-  let cartItemContainer = document.querySelector(".cart-items");
-  let cartRows = cartItemContainer.querySelectorAll(".cart-row");
+  const cartItemContainer = document.querySelector(".cart-items");
+  const cartRows = cartItemContainer.querySelectorAll(".cart-row");
   let total = 0;
   for (let i = 0; i < cartRows.length; i++) {
     let cartRow = cartRows[i];
